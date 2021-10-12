1ea145060bfdf57cbda3f08dceb95c85845b2a0b
Kalil565 <91091960+Kalil565@users.noreply.github.com>
2021-10-12 03:22:08 -0300

    SegundaInterface(Conectada)

--- Telas.py
+++ Telas.py
@@ -6,11 +6,31 @@ def telaDeLogin():
     login = [
         [sg.Text("Usuário"), sg.Input(size= (40, 0), key= 'usuarioLogado')],
         [sg.Text("Senha  "), sg.Input(size= (40, 0), key= 'senhaLogado')],
-        [sg.Button("Cadastrar", size= (7, -1), key= 'Cadastrar'), sg.Text(" " * 55), sg.Button("Logar", size= (5, -1), key= 'Logar')]
+        [sg.Checkbox("Lembre-me?", key= 'lembrar')],
+        [sg.Button("Cadastrar", size= (7, -1), key= 'Cadastrar'), sg.Text(" " * 65), sg.Button("Logar", size= (5, -1), key= 'Logar')]
     ]
-    janelaDeLogin = sg.Window("Login", layout= login, size= (400, 150))
+    janelaDeLogin = sg.Window("Login", layout= login, size= (450, 150))
 
     button, values = janelaDeLogin.Read()
+    
+    if button == 'Cadastrar':
+        janelaDeLogin.close()
+        telaDeCadastro()
+
+def telaDeCadastro():
+    sg.theme('Black')
+
+    cadastro = [
+        [sg.Text("Nome de Usuário"), sg.Input(size= (45, 0), key= 'usuarioCadastrar')],
+        [sg.Text("Senha "), sg.Input(size= (55, 0), key= 'senhaCadastrar')],
+        [sg.Text('')],
+        [sg.Button("Voltar", size= (7, 0), key= 'voltarLogin'), sg.Text(' ' * 65), sg.Button("Continuar", size= (10, 0), key= 'continuarCadastro')]
+    ]
+
+    janelaDeCadastro= sg.Window("Cadastro", layout= cadastro, size= (450, 150))
+
+    button, values = janelaDeCadastro.Read()
+
 
 
 
